9c0cea29efe3bb91c758f75774106a550a803e6c
laidhimonthegreen <mpodr2015@gmail.com>
2018-09-10 20:36:18 +0300

    another one

--- flex.py
+++ flex.py
@@ -1,21 +1,23 @@
 
 
 
+
 while True:
     slovo = input("Введите слово для анализа, прилагательное мужского рода: ")
     if slovo:       
         if slovo.endswith("ый") or slovo.endswith ("ой") or slovo.endswith ("ий"):
-            print ("Нам кажется, это именительный или винительный падеж")
+            rezult = "именительный или винительный падеж"
         elif slovo.endswith ("его") or slovo.endswith ("ого"):
-            print ("Нам кажется, это родительный падеж")
+            rezult = "родительный падеж"
         elif slovo.endswith ("ому") or slovo.endswith ("ему"):
-            print ("Нам кажется, это дательный падеж")
+            rezult = "дательный падеж"
         elif slovo.endswith ("им") or slovo.endswith ("ым"):
-            print ("Нам кажется, это творительный падеж")
+            rezult = "творительный падеж"
         elif slovo.endswith ("ом") or slovo.endswith ("ем"):
-            print ("Нам кажется, это предложный падеж")
+            rezult = "предложный падеж"
         else:
-            print ("Мы ничего не понимаем")
+            rezult = "анализ ничего не показал"
+        print ("Результат анализа:", rezult)
     else:
         break
     
