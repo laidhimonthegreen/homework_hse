97f75eb6b94b77701638cbc9f04c8ca44d467a95
laidhimonthegreen <mpodr2015@gmail.com>
2018-09-13 14:08:06 +0300

    folders

---
+++ classwork/flex.py
@@ -0,0 +1,54 @@
+while True:
+    slovo = input("Введите слово для анализа, прилагательное мужского рода: ")
+    if slovo:       
+        if slovo.endswith("ый") or slovo.endswith ("ой") or slovo.endswith ("ий"):
+            rezult = "именительный или винительный падеж"
+        elif slovo.endswith ("его") or slovo.endswith ("ого"):
+            rezult = "родительный падеж"
+        elif slovo.endswith ("ому") or slovo.endswith ("ему"):
+            rezult = "дательный падеж"
+        elif slovo.endswith ("им") or slovo.endswith ("ым"):
+            rezult = "творительный падеж"
+        elif slovo.endswith ("ом") or slovo.endswith ("ем"):
+            rezult = "предложный падеж"
+        else:
+            rezult = "анализ ничего не показал"
+        print ("Результат анализа:", rezult)
+    else:
+        break
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
