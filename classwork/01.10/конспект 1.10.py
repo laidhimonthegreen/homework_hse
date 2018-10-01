199db89873e6a1b9bea4d296f0b45c8b3f73c2dd
laidhimonthegreen <mpodr2015@gmail.com>
2018-10-01 18:59:43 +0300

    а

--- classwork/01.10/конспект 1.10.py
+++ classwork/01.10/конспект 1.10.py
@@ -12,3 +12,15 @@ print(alph)
 
 #чтение и запись
 f = open("file.txt")
+#относительный путь для вложенной директории
+"dir1/file.txt"
+#абсолютный
+r"С:\Users\student\file.txt"
+"С:\\Users\\student\\file.txt"
+#если есть обратный слэш то питон сходит с ума
+s = "абс\"de"
+\t - табуляция
+#относительный
+"../file.txt"
+filename = "file.txt"
+f = open(filename, "r", encoding = "utf-8")
