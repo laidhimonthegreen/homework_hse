2a16d34da96ac5b32802c71cee46b149b7c59b42
laidhimonthegreen <mpodr2015@gmail.com>
2018-11-05 19:13:07 +0300

    hw04 update

--- homework/HW04/HW04.py
+++ homework/HW04/HW04.py
@@ -12,17 +12,16 @@ def kebabter(text1, text2):
     with open(text1, "r", encoding="utf-8") as f:
         with open(text2, "w", encoding="utf-8") as f2:
             for line in f.readlines():
-                line = (re.sub(r'язык([ауеи(ом)(ам)(ах)(ов)])\b', r'шашлык\1', line))
-                line = (re.sub(r'Язык([ауеи(ом)(ам)(ах)(ов)]\b)', r'Шашлык\1', line))
-                line = (re.sub('язык\b', r'шашлык', line))  
-                line = (re.sub('Язык\b', r'Шашлык', line))
+                line = (re.sub(r'\bязык([ауеи]|(ом)|(ам)|(ах)|(ов)|(ами)]){,1}\b', r'шашлык\1', line))
+                line = (re.sub(r'\bЯзык([ауеи]|(ом)|(ам)|(ах)|(ов)|(ами)]){,1}\b', r'Шашлык\1', line))
                 f2.write(line)
+
 
 def astrologizator(text1, text2):
     with open(text1, "r", encoding="utf-8") as f:
         with open(text2, "w", encoding="utf-8") as f2:
             for line in f.readlines():
-                line = (re.sub(r'философи([яиюй(ей)(ям)(ях)])', r'астрологи\1', line))
+                line = (re.sub(r'\bфилософи([яиюй(ей)(ям)(ях)])', r'астрологи\1', line))
                 line = (re.sub(r'Философи([яиюй(ей)(ям)(ях)])', r'Астрологи\1', line))
                 line = (re.sub('ФИЛОСОФИЯ','АСТРОЛОГИЯ', line))
                 line = (re.sub('Филосо́фия','Астрология', line))
